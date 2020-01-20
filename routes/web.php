e6bd9b7f3967403a21c300513430248a02a1d171
MichaelEgan <egan16@gmail.com>
2020-01-20 15:19:51 +0000

    made user crud routes for lists, made ListController

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin/home', 'Admin\HomeController@index')->name('admin.home');
 Route::get('/user/home', 'User\HomeController@index')->name('user.home');
 
-//CRUD routes for tables go here
+//CRUD routes for user
+//User lists crud
+Route::get('/user/lists', 'User\ListController@index')->name('user.lists.index');
+Route::get('/user/lists/create', 'User\ListController@create')->name('user.lists.create');
+Route::get('/user/lists/{id}', 'User\ListController@show')->name('user.lists.show');
+Route::post('/user/lists/store', 'User\ListController@store')->name('user.lists.store');
+Route::get('/user/lists/{id}/edit', 'User\ListController@edit')->name('user.lists.edit');
+Route::put('/user/lists{id}', 'User\ListController@update')->name('user.lists.update');
+Route::delete('/user/lists{id}', 'User\ListController@destroy')->name('user.lists.destroy');
